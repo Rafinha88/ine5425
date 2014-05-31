2419e4bffd30f23f0f93271bba7ba7854cab7981
Thiago Rose <thiago.senhorinha@gmail.com>
2014-05-31 12:11:01 -0300

    Adicionando e implementando classe principal da simulação.

---
+++ src/modelo/Simulador.java
@@ -0,0 +1,48 @@
+package modelo;
+
+
+public class Simulador {
+
+	// TODO: Adicionar os atributos recebidos pela view de configuração do
+	// operador.
+
+	static Relogio relogio;
+	static GeradorDeChamada geradorDeChamada;
+	static Estatistica estatistica;
+	static ListaEncadeadaOrdenada<Evento> eventos;
+
+	public static void main(String[] args) {
+		setup();
+		loop();
+	}
+
+	private static void setup() {
+		eventos = new ListaEncadeadaOrdenada<>();
+		relogio = new Relogio();
+		estatistica = new Estatistica();
+		// TODO: Adicionar gerador com os dados da view
+		geradorDeChamada = null;
+	}
+
+	private static void loop() {
+		// TODO: Implementar pausa etc.
+		while (true) {
+			avancarTempo();
+			executarEvento();
+			mostrarEstatisticas();
+		}
+	}
+
+	private static void avancarTempo() {
+
+	}
+
+	private static void executarEvento() {
+
+	}
+
+	private static void mostrarEstatisticas() {
+
+	}
+
+}
